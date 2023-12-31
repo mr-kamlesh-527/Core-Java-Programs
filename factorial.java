a759f2fc72f8768c44028e5d17a2a2487eaf1e49
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2023-12-31 21:24:55 +0530

    factorial program added

---
+++ factorial.java
@@ -0,0 +1,11 @@
+class Factorial {
+public static void main(String [] args){
+int n, fact=1;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a Number : ")
+for(int i=0; i<=n; i++){
+      fact = n*i
+}
+System.out.println("factorial of "+n+" is = "+ fact)
+}
+}
