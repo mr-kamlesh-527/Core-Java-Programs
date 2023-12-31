a3880adb0b4a416727dd8923a27b2916fab6769d
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2023-12-31 21:25:55 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -3,6 +3,8 @@ public static void main(String [] args){
 int n, fact=1;
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter a Number : ")
+n = sc. nextInt();
+
 for(int i=0; i<=n; i++){
       fact = n*i
 }
