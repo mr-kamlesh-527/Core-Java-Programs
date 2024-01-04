dc5e4debd018b2c5278b2f91178f907b2fee4a5f
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-01-04 21:40:14 +0530

    Create Alphabet.java

---
+++ Core java Program/Alphabet.java
@@ -0,0 +1,16 @@
+public class Alphabet {
+	public static void main(String[] args) {
+        // print lowercase alphabets
+        System.out.println("lowercase alphabets : ");
+        char i;
+        for(i = 'a'; i <= 'z'; i++){
+          System.out.print(i+" ");
+        }System.out.println();
+        //Print Uppercase alphabets
+        System.out.println("UPPERCASE ALPHABETS:");
+        char uppercaseAlphabet;
+        for(i = 'A'; i <= 'Z'; i++){
+          System.out.print(i+" ");
+        }
+	}
+}
