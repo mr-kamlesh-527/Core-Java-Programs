60752574238d39605d6ed9090a20bfdeb796e0ee
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-01-04 22:17:24 +0530

    Added AreaOfTriangle.java

---
+++ Core java Program/AreaOfTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+	public static void main(String[] args) {
+		int a,b,c, sp;
+		double areaoftriangle;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Triangle sides : ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=sc.nextInt();
+		//sp== semiparameter
+		sp = (a+b+c)/2;
+		areaoftriangle = Math.sqrt(sp*(sp-a)*(sp-b)*(sp-c));
+		System.out.println("Area Of Triangle = "+areaoftriangle);
+	}
+}
