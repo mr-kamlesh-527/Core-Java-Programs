75c8c4b02b6a575d33d230cd578f9fbe0de0d2b5
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-01-16 13:01:35 +0530

    Update Array2.java

--- Core java Program/Array Programs/Array2.java
+++ Core java Program/Array Programs/Array2.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+
+public class Array2{
+    public static void main(String[] args) {
+      //this is dynamic array declaration
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Size of Array: ");
+        n = sc.nextInt();
+        int a[] =new int[n];
+      
+        System.out.print("Enter Array Elements : ");
+        for(int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+        System.out.print("\nArray Elements  : ");
+        for(int b:a){
+            System.out.print(b+" ");
+        }
+
+    }
+}
 
