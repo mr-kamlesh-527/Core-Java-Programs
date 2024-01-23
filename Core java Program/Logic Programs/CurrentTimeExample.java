07ec20c1081db545a10ed61376aa0893a6ead5a9
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-01-24 00:06:38 +0530

    Create CurrentTimeExample.java

---
+++ Core java Program/Logic Programs/CurrentTimeExample.java
@@ -0,0 +1,11 @@
+import java.time.LocalTime;
+
+public class CurrentTimeExample {
+    public static void main(String[] args) {
+        // Get the current time
+        LocalTime currentTime = LocalTime.now();
+
+        // Print the current time
+        System.out.println("Current Time: " + currentTime);
+    }
+}
