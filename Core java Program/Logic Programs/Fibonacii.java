afcec16a887e4f1bd4f43760d9643c68ca41b0dd
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-02-02 05:08:58 +0530

    Update and rename Fibanacii.java to Fibonacii.java

---
+++ Core java Program/Logic Programs/Fibonacii.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Fibonacii {
+	public static void main(String[] args) {
+		int a=0, b=1, c=0, term;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a Number : ");
+		term = sc.nextInt();
+		for(int i=1; i<=term; i++) {
+			System.out.print(a+" ");
+			c=a+b;
+			a=b;
+			b=c;
+		}
+	}
+}
+
