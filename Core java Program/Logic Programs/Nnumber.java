7577f979afa80876e3e0050414d24a9531701a6c
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-02-09 11:50:17 +0530

    Update Nnumber.java

--- Core java Program/Logic Programs/Nnumber.java
+++ Core java Program/Logic Programs/Nnumber.java
@@ -1,12 +1,1 @@
-import java.util.Scanner;
 
-public class Nnumber {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a Natural number : ");
-		int n = sc.nextInt();
-		for(int i=1; i<=n; i++) {
-			System.out.println(i);
-		}
-  }
-}
