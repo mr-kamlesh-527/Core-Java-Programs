6dbe7fb5f0b1526c937f1f0bd192655c08c4dfc6
Chavan Kamlesh <154850954+mr-kamlesh-527@users.noreply.github.com>
2024-02-18 10:10:46 +0530

    Create objectcloning.java

---
+++ Core java Program/Logic Programs/objectcloning.java
@@ -0,0 +1,27 @@
+class objectcloning implements Cloneable {
+	int rollno;
+	String name;
+
+	objectcloning(int rollno, String name) {
+		this.rollno = rollno;
+		this.name = name;
+	}
+
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	public static void main(String args[]) {
+		try {
+			objectcloning s1 = new objectcloning(101, "kamlesh");
+
+			objectcloning s2 = (objectcloning) s1.clone();
+
+			System.out.println(s1.rollno + " " + s1.name);
+			System.out.println(s2.rollno + " " + s2.name);
+
+		} catch (CloneNotSupportedException c) {
+		}
+
+	}
+}
